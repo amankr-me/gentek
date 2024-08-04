e4db746e3445dba0f6d3953619a4464895ecd8f7
amankr-me <58977422+amankr-me@users.noreply.github.com>
2024-08-05 00:10:00 +0530

    Done

--- about.js
+++ about.js
@@ -21,12 +21,12 @@
 
     });
     // fixed footer
-    var siteFooter = document.getElementById('site-footer');
-    if ((siteFooter.offsetTop + siteFooter.offsetHeight) < window.innerHeight) {
-        siteFooter.classList.add('fixed-bottom', 'bottom-0', 'left-0', 'w-full');
-    }
+    // var siteFooter = document.getElementById('site-footer');
+    // if ((siteFooter.offsetTop + siteFooter.offsetHeight) < window.innerHeight) {
+    //     siteFooter.classList.add('fixed-bottom', 'bottom-0', 'left-0', 'w-full');
+    // }
     /* Animate On Scroll */
-    AOS.init();
+    // AOS.init();
     /* Owl Carousel */
     $('.owl-carousel').owlCarousel({
         loop: true,
