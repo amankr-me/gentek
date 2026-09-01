e16c9ccb592cf2add9a366e5862218b5142861c2
amankr-questglt <amankumar@questglt.org>
2026-09-02 01:57:12 +0530

    redesign

--- menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.zIndex = "100";
-  document.getElementById("mySidenav").style.width = "250px";
-
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
